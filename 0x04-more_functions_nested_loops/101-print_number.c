b60aa3b75a059b6501ee756e4add6ed5f5d7da20
abayi6 <yaeyram@gmail.com>
2022-03-26 18:43:02 -0700

    first advanced task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,17 +4,3 @@
  * @n: print int
  * Return: Always 0.
  */
-void print_number(int n)
-{
-	print_number(98):
-		_putchar('\n');
-    print_number(402);
-        _putchar('\n');
-	    print_number(1024);
-	        _putchar('\n');
-		    print_number(0);
-		        _putchar('\n');
-			    print_number(-98);
-			        _putchar('\n');
-				    return (0);
-				    }
